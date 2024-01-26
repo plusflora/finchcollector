b26e0dfb7f8384568a4a380b28dd4861d7077543
vbouq <v.bouquet707@gmail.com>
2024-01-26 05:36:08 -0800

    adds feedings

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
 # Create your models here.
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+) 
 
 # finch model
 class Finch(models.Model):
@@ -13,3 +20,17 @@ class Finch(models.Model):
   def get_absolute_url(self):
     return reverse('detail', kwargs={'finch_id': self.id})
 #
+  
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date} for {self.finch}"
+  class Meta:
+    ordering = ['-date']
+
